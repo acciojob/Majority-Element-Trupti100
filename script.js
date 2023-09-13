ae66506de69e00b45ccd03b5c565d45b92e996ab
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-13 08:15:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your code here
+function findMajorityElement(nums) {
+    let candidate = nums[0];
+    let count = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+
+        if (count === 0) {
+            candidate = nums[i];
+            count = 1;
+        }
+    }
+
+    return candidate;
+}
+
+// Test the function
+const nums = [2, 1, 2];
+const majorityElement = findMajorityElement(nums);
+console.log(majorityElement); // Output: 2
+
